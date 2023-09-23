e2becd07454ee9739c014f3d27d7ff423b9ba082
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-09-23 23:57:00 +0200

    feat: Add update and delete actions to imperative client

--- frontend/api/rest.ts
+++ frontend/api/rest.ts
@@ -14,11 +14,53 @@ export class ConfigurationRestAPI {
       service: this.service,
     }).toString();
 
+    const response = await fetch(configurationURL.toString(), {
+      headers: {
+        Authorization: "Bearer " + this.accessJWT,
+      },
+    });
+
+    if (response.status === 404) {
+      return this.updateConfiguration({
+        enabled: false,
+        postTTL: 6,
+      });
+    }
+
+    return response.json();
+  }
+
+  async updateConfiguration(config: IConfiguration): Promise<IConfiguration> {
+    const configurationURL = new URL(this.apiURL + "configuration");
+
+    configurationURL.search = new URLSearchParams({
+      service: this.service,
+    }).toString();
+
     return (
       await fetch(configurationURL.toString(), {
+        method: "PUT",
+        body: JSON.stringify(config),
         headers: {
           Authorization: "Bearer " + this.accessJWT,
           "Content-Type": "application/json",
+        },
+      })
+    ).json();
+  }
+
+  async deleteConfiguration() {
+    const configurationURL = new URL(this.apiURL + "configuration");
+
+    configurationURL.search = new URLSearchParams({
+      service: this.service,
+    }).toString();
+
+    return (
+      await fetch(configurationURL.toString(), {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer " + this.accessJWT,
         },
       })
     ).json();
